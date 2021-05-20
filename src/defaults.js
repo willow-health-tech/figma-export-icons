ba4de4b564f2f47db0ce695d4c1a40e418e92313
John Williams <john@meiday.health>
2021-05-20 17:04:20 -0400

    writing out index.(j|t)sx? files on gen

--- src/defaults.js
+++ src/defaults.js
@@ -2,7 +2,8 @@ const defaults = {
   configFileName: 'icons-config.json',
   page: 'Identity',
   frame: 'Icons',
-  iconsPath: 'assets/svg/icons'
+  iconsPath: 'assets/svg/icons',
+  indexExt: 'ts'
 }
 
 module.exports = defaults
